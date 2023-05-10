84356b47153a6e87b1e2f12c300bbe70d9cb438c
Peter Kristensen <peter@ptx.dk>
2023-05-10 17:24:41 +0200

    WIP, special backend

--- backend/glfwbackend/glfw_backend.cpp
+++ backend/glfwbackend/glfw_backend.cpp
@@ -90,7 +90,7 @@ GLFWwindow *igCreateGLFWWindow(const char *title, int width, int height,
   // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad
   // Controls
   io->ConfigFlags |= ImGuiConfigFlags_DockingEnable;   // Enable Docking
-  io->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Enable Multi-Viewport
+  //io->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Enable Multi-Viewport
                                                        // / Platform Windows
   // io.ConfigViewportsNoAutoMerge = true;
   // io.ConfigViewportsNoTaskBarIcon = true;
